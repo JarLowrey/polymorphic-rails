188d6160dfdae97f9d5cbc4a5dd824eab46ffcd8
James <jtronlabs@gmail.com>
2017-07-13 17:17:01 -0400

    better way of making polymorphic work

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -27,7 +27,8 @@ class EventsController < ApplicationController
   # POST /events.json
   def create
     @event = Event.new(event_params)
-
+    @event.address.addressable = @event
+    
     respond_to do |format|
       if @event.save
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,3 @@
 class Address < ApplicationRecord
-  belongs_to :addressable, polymorphic: true, optional:true
+  belongs_to :addressable, polymorphic: true
 end
